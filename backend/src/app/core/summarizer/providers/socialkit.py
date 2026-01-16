11946d90928204196b687581e4413c6510b30023
Alex <appwork1020@gmail.com>
2026-01-16 15:44:20 +0100

    update gitignore and toml cleaning

--- backend/src/app/core/summarizer/providers/socialkit.py
+++ backend/src/app/core/summarizer/providers/socialkit.py
@@ -1,1 +1,16 @@
-# TODO implement a function that calls SocialKit and prints the raw JSON
+import requests
+
+def request_socialkit_transcript(url, key):
+    # TODO implement a function that calls SocialKit and prints the raw JSON
+    
+    response = requests.get(f'https://api.socialkit.dev/youtube/transcript?access_key={key}&url={url}')
+    print(response.text)
+    ...
+    
+def main():
+    test_url = "https://www.youtube.com/watch?v=Ir9QYpHeRAc&t=2s"
+    result = request_socialkit_transcript(test_url)
+    print(result)
+    
+if __name__ == "__main__":
+    main()
