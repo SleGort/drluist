17f0f6afe77a86b71150198f52a951d1da6143c3
Alex <appwork1020@gmail.com>
2026-01-16 15:59:46 +0100

    added env file for security

--- backend/src/app/core/summarizer/providers/socialkit.py
+++ backend/src/app/core/summarizer/providers/socialkit.py
@@ -1,4 +1,6 @@
 import requests
+import os
+from dotenv import load_dotenv
 
 def request_socialkit_transcript(url, key):
     # TODO implement a function that calls SocialKit and prints the raw JSON
@@ -9,7 +11,13 @@ def request_socialkit_transcript(url, key):
     
 def main():
     test_url = "https://www.youtube.com/watch?v=Ir9QYpHeRAc&t=2s"
-    result = request_socialkit_transcript(test_url)
+    load_dotenv()
+    access_key = os.getenv("SOCIALKIT_API_KEY")
+    
+    if not access_key:
+        raise RuntimeError("SOCIALKIT_API_KEY is not set!")
+    
+    result = request_socialkit_transcript(test_url, access_key)
     print(result)
     
 if __name__ == "__main__":
